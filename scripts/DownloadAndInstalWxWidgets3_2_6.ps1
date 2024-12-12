ba17d386e0fb6399aed34c1a3e56fc1be7f9bba6
milton <miltonmcdonald1986@gmail.com>
2024-12-12 09:25:19 -0500

    Fixed up wxWidgets install script to download headers and binaries.

--- scripts/DownloadAndInstalWxWidgets3_2_6.ps1
+++ scripts/DownloadAndInstalWxWidgets3_2_6.ps1
@@ -1,20 +1,9 @@
-$zipPath = "..\lib\wxWidgets-3.2.6.zip"
-Invoke-WebRequest https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.6/wxWidgets-3.2.6.zip -OutFile $zipPath
+# First download headers
+Invoke-WebRequest https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.6/wxWidgets-3.2.6-headers.7z -OutFile "$env:TEMP\wxWidgets-3.2.6-headers.7z"
 
-$unzipPath = $FilePath + "\wxWidgetsTEMP"
-Expand-Archive -Path $zipPath -DestinationPath $unzipPath
+# Next, download binaries
+Invoke-WebRequest https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.6/wxMSW-3.2.6_vc14x_x64_ReleaseDLL.7z -OutFile "$env:TEMP\wxMSW-3.2.6_vc14x_x64_ReleaseDLL.7z"
 
-$libPath = $unzipPath + "\wxWidgets-3.2.6"
-Move-Item $libPath $FilePath
-
-Remove-Item $zipPath
-Remove-Item $unzipPath
-
-$realPath = $FilePath + "wxWidgets-3.2.6\"
-[System.Environment]::SetEnvironmentVariable('WXWIN_3_2_6', $realPath, 'User')
-	
-cd $realPath
-cd build
-cd msw
-nmake /f makefile.vc BUILD=debug SHARED=1 TARGET_CPU=x64
-nmake /f makefile.vc BUILD=release SHARED=1 TARGET_CPU=x64
+# Unpack the stuff, assumes the current directory is `scripts`
+..\lib\7-Zip\7z.exe e "$env:TEMP\wxWidgets-3.2.6-headers.7z" "-o..\lib\wxWidgets-3.2.6\include\" -aoa
+..\lib\7-Zip\7z.exe e "$env:TEMP\wxMSW-3.2.6_vc14x_x64_ReleaseDLL.7z" "-o..\lib\wxWidgets-3.2.6\lib\" -aoa
