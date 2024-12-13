7569ca06072ed8348eee39cfb746853c32692387
milton <miltonmcdonald1986@gmail.com>
2024-12-13 08:52:41 -0500

    Renamed file that I spelled Install wrong, and changed the way I try to execute the other scripts.

--- scripts/DownloadAndInstallAllDependencies.ps1
+++ scripts/DownloadAndInstallAllDependencies.ps1
@@ -1,3 +1,3 @@
-& "DownloadAndInstall7Zip2409.ps1"
-& "DownloadAndInstallWxWidgets3_2_6.ps1"
-& "DownloadAndInstallWxFormBuilder4_2_1.ps1"
+./DownloadAndInstall7Zip2409.ps1
+./DownloadAndInstallWxWidgets3_2_6.ps1
+./DownloadAndInstallWxFormBuilder4_2_1.ps1
