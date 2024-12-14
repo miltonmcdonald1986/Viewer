83c6128a17ae2b662a1a94b6bf0ff811940faa62
milton <miltonmcdonald1986@gmail.com>
2024-12-14 14:53:17 -0500

    Broken commit but making progress.

--- scripts/DownloadAndInstallWxWidgets3_2_6.ps1
+++ scripts/DownloadAndInstallWxWidgets3_2_6.ps1
@@ -1,9 +1,13 @@
-# First download headers
+# First download the header files
 Invoke-WebRequest https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.6/wxWidgets-3.2.6-headers.7z -OutFile "$env:TEMP\wxWidgets-3.2.6-headers.7z"
 
-# Next, download binaries
+# Then, download the development files
+Invoke-WebRequest https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.6/wxMSW-3.2.6_vc14x_x64_Dev.7z -OutFile "$env:TEMP\wxMSW-3.2.6_vc14x_x64_Dev.7z"
+
+# Finally, download the Release DLLs
 Invoke-WebRequest https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.6/wxMSW-3.2.6_vc14x_x64_ReleaseDLL.7z -OutFile "$env:TEMP\wxMSW-3.2.6_vc14x_x64_ReleaseDLL.7z"
 
 # Unpack the stuff, assumes the current directory is `scripts`
-..\lib\7-Zip\7z.exe e "$env:TEMP\wxWidgets-3.2.6-headers.7z" "-o..\lib\wxWidgets-3.2.6\include\" -aoa
-..\lib\7-Zip\7z.exe e "$env:TEMP\wxMSW-3.2.6_vc14x_x64_ReleaseDLL.7z" "-o..\lib\wxWidgets-3.2.6\lib\" -aoa
+..\lib\7-Zip\7z.exe x "$env:TEMP\wxWidgets-3.2.6-headers.7z" "-o..\lib\wxWidgets-3.2.6\" -aoa
+..\lib\7-Zip\7z.exe x "$env:TEMP\wxMSW-3.2.6_vc14x_x64_Dev.7z" "-o..\lib\wxWidgets-3.2.6\" -aoa
+..\lib\7-Zip\7z.exe x "$env:TEMP\wxMSW-3.2.6_vc14x_x64_ReleaseDLL.7z" "-o..\lib\wxWidgets-3.2.6\" -aoa
