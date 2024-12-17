d39d361d0fee73e1e9d31c78d3944e3037e3c454
milton <miltonmcdonald1986@gmail.com>
2024-12-17 09:23:24 -0500

    Fixed issue when installing dependencies.

--- scripts/DownloadAndInstallWxFormBuilder4_2_1.ps1
+++ scripts/DownloadAndInstallWxFormBuilder4_2_1.ps1
@@ -2,4 +2,5 @@
 Invoke-WebRequest https://github.com/wxFormBuilder/wxFormBuilder/releases/download/v4.2.1/wxFormBuilder-4.2.1-x64.exe -OutFile "$env:TEMP\wxFormBuilder-4.2.1-x64.exe"
 
 # Install it
-& "$env:TEMP\wxFormBuilder-4.2.1-x64.exe" "/VERYSILENT" "/DIR=C:\Users\milto\source\repos\Viewer\lib\wxFormBuilder-4.2.1-x64"
+$lib = "$pwd\..\lib"
+& "$env:TEMP\wxFormBuilder-4.2.1-x64.exe" "/VERYSILENT" "/DIR=$lib\wxFormBuilder-4.2.1-x64"
